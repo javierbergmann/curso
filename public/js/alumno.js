645605d407aecc8e508cb7812e0dd8d18f6834c6
javierbergmann <javier.bergmann@gmail.com>
2022-05-12 08:34:00 -0300

    gg

---
+++ public/js/alumno.js
@@ -0,0 +1,11 @@
+window.onload= (e)=> e.preventDefault();
+
+let buttonSave= document.getElementById("btnGrabar");
+let buttonCancel= document.getAnimations("btnCancelar");
+buttonSave.addEventListener("click", function(e){
+    e.preventDefault();
+    alert("btnGrabar");
+})
+
+
+
